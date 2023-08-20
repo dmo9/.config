f4e2f74f2a4db78b8d5b55ea2921b820f765f7f1
dob <dmo9@vt.edu>
2023-08-20 18:51:29 -0400

    shortened wikijs func to wiki & cleaned up

--- fish/functions/wiki.fish
+++ fish/functions/wiki.fish
@@ -2,7 +2,7 @@
 # file: ~/.config/fish/functions/wikijs.fish
 # purpose: launch wikijs in an app-like firefox window
 
-function wikijs
+function wiki
 
     # launch firefox browser w/o tabs or search bar
     firefox localhost:3000 -P "no-tabs-or-search-bar" &;
