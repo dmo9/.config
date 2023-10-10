d94daab39856fc559ee7ee74d1cdf2256eff20a6
= <=>
2023-10-10 18:23:41 -0400

    yeet

--- scripts/get_config_repo.sh
+++ scripts/get_config_repo.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# this script is used to pull in the latest configuration
+# files, scripts, and package lists for use with a fresh install 
 git clone https://github.com/dmo9/linux_config
